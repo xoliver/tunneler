e5d431d08638cbdce1ce04ff68672ca9ebea2ce6
Mark Smith <mark.smith@practicalpoetry.co.uk>
2016-03-08 13:55:16 +0000

    Added tests for get_configured_groups.

--- tunneler/tests/test_tunneler.py
+++ tunneler/tests/test_tunneler.py
@@ -11,6 +11,10 @@ from ..tunneler import (
 )
 
 
+def is_tunnel_active_stub(name):
+    return name.startswith('active')
+
+
 class CheckTunnelExistsTestCase(TestCase):
     def test_tunnel_exists(self):
         func = Mock()
@@ -68,6 +72,45 @@ class TunnelerTestCase(TestCase):
             tunnels=tunnel_config,
             groups=group_config
         )
+
+        # Used for testing get_configured_groups. Tunnels are only made active,
+        # or inactive by stubbing out 'is_tunnel_active' with a function that
+        # does name matching:
+        self.complex_config = Configuration(
+            common={'default_user': 'testuser'},
+            tunnels={
+                'active_tunnel1': {
+                    'user': self.tunnel.user,
+                    'server': self.tunnel.server,
+                    'local_port': self.tunnel.local_port,
+                    'remote_port': self.tunnel.remote_port,
+                },
+                'active_tunnel2': {
+                    'user': self.tunnel.user,
+                    'server': self.tunnel.server,
+                    'local_port': self.tunnel.local_port,
+                    'remote_port': self.tunnel.remote_port,
+                },
+                'inactive_tunnel1': {
+                    'user': self.tunnel.user,
+                    'server': self.tunnel.server,
+                    'local_port': self.tunnel.local_port,
+                    'remote_port': self.tunnel.remote_port,
+                },
+                'inactive_tunnel2': {
+                    'user': self.tunnel.user,
+                    'server': self.tunnel.server,
+                    'local_port': self.tunnel.local_port,
+                    'remote_port': self.tunnel.remote_port,
+                }
+            },
+            groups={
+                'all_active': [('active_tunnel1', None), ('active_tunnel2', None)],
+                'all_inactive': [('inactive_tunnel1', None), ('inactive_tunnel2', None)],
+                'mixed': [('active_tunnel1', None), ('inactive_tunnel1', None)],
+            }
+        )
+
         self.empty_config = Configuration({}, {}, {})
 
         self.tunneler = Tunneler(self.process_helper, self.empty_config)
@@ -87,6 +130,24 @@ class TunnelerTestCase(TestCase):
     def test_get_configured_groups(self):
         self.tunneler.config = self.config
         self.assertEqual([self.group_name], self.tunneler.get_configured_groups())
+
+    def test_get_configured_groups_active(self):
+        self.tunneler.config = self.complex_config
+
+        with patch.object(self.tunneler, 'is_tunnel_active', is_tunnel_active_stub):
+            self.assertEqual(
+                ['all_active'],
+                self.tunneler.get_configured_groups(True),
+            )
+
+    def test_get_configured_groups_inactive(self):
+        self.tunneler.config = self.complex_config
+
+        with patch.object(self.tunneler, 'is_tunnel_active', is_tunnel_active_stub):
+            self.assertEqual(
+                ['all_inactive'],
+                self.tunneler.get_configured_groups(False),
+            )
 
     def test_identify_tunnel_with_one_found(self):
         name = 'testserver'
